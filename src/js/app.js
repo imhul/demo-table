4a32a8ecbb4a264ad1dbea6b09f095af27da9bf2
tkachuk <z.tkachuk@nodeart.io>
2023-11-24 16:48:35 +0200

    reducing the number of imported fonts

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 // app.js
 import { createTable } from './table.js'
 import { handleSignUp } from './signUp.js'
-import '@fontsource/roboto'
+import '@fontsource/roboto/latin-400.css'
+import '@fontsource/roboto/latin-700.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   createTable()
